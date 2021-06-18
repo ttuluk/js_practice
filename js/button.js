53c5f6181184f5f7512cb7eb4518e59c56657e97
ttuluk <ttasya.k@gmail.com>
2021-06-18 16:01:42 +0300

    add button

--- js/button.js
+++ js/button.js
@@ -1,16 +1,24 @@
 const btnAdd = document.querySelector('button');
-const valueInput = document.querySelector('input[data-value]')
-
+const valueInput = document.querySelector('input')
+const outputEl = document.querySelector('.js-output span');
+let total = 0;
 
 console.dir(btnAdd);
 
 btnAdd.textContent = 'goIt';
 btnAdd.addEventListener('click', function () {
     console.log('click hehehehe');
-    console.log(valueInput.value);
-})
-console.dir(valueInput);
 
+    const value = Number(valueInput.value);
+    console.log(value);
+    total += value;
+    console.log(typeof total);
+    outputEl.textContent = total;
+    valueInput.value = '';
+    console.log(total);
+})
+
+/* console.dir(valueInput);
 valueInput.addEventListener('input', function () {
     console.log('you have value');
-})
+}) */
